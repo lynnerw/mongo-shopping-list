53e75f34763d1d3bd3865245ddc612a782a2157e
Lynne <larwitter@gmail.com>
2016-11-11 00:04:32 +0000

    reverted config.js to pass tests

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-var mlab_URL = 'mongodb://mlabdb:2SunSky&Luv@ds139937.mlab.com:39937/thinkfullyn';
-exports.DATABASE_URL = mlab_URL ||
-                       process.env.DATABASE_URL ||
+//var mlab_URL = 'mongodb://mlabdb:2SunSky&Luv@ds139937.mlab.com:39937/thinkfullyn'; mlab_URL ||
+exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
